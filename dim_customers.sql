95f9cba25761e137d0d3d76ce9e990b589229d02
stupham2k2 <thanhhuongpham2002@gmail.com>
2026-08-20 23:58:27 +0200

    Add files via upload

---
+++ dim_customers.sql
@@ -0,0 +1,14 @@
+SELECT 
+    c.CustomerID,
+    c.CustomerName,
+    c.Email,
+    c.Gender,
+    c.Age,
+    g.Country,
+    g.City
+FROM 
+    dbo.customers as c
+LEFT JOIN
+    dbo.geography g
+ON 
+    c.GeographyID = g.GeographyID;
